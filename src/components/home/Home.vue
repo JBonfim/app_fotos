b54815949f838d3f5963f22e41c5e9bcc6116d78
JBonfim <jabes.bonfim@dce.ufpb.br>
2019-09-01 22:46:51 -0300

    Adicionando componente e novos recursos

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -8,6 +8,12 @@
       <li class="lista-fotos-item" v-for="foto of fotosComFiltro" v-bind:key="foto._id">
         <meu-painel :titulo="foto.titulo">
              <imagem-responsiva :url="foto.url" :titulo="foto.titulo"/>
+             <meu-botao 
+                rotulo="remover" 
+                tipo="button" 
+                :confirmacao="true" 
+                @botaoAtivado="remove(foto)"
+                estilo="perigo"/>
         </meu-painel>
       </li>
     </ul>
@@ -18,10 +24,19 @@
 <script>
 import Painel from '../shared/painel/Painel.vue';
 import ImagemResponsiva from '../shared/imagem-responsiva/ImagemResponsiva.vue'
+import Botao from '../shared/botao/Botao.vue';
 export default {
+    
   components:{
     'meu-painel': Painel,
-    'imagem-responsiva': ImagemResponsiva
+    'imagem-responsiva': ImagemResponsiva,
+     'meu-botao': Botao
+  },
+  methods: {
+
+    remove(foto) {
+       alert(foto.titulo);
+    }
   },
  data(){
    return{
