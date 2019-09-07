2ee683b85a43ec29b91bc42ff47b1a5230ad62d1
JBonfim <jabes.bonfim@dce.ufpb.br>
2019-09-07 08:51:44 -0300

    Tratando erros de requisição da aplicação

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -50,10 +50,7 @@ export default {
             this.fotos.splice(indice, 1);
             this.mensagem = 'Foto removida com sucesso'
           }, 
-          err => {
-            this.mensagem = 'Não foi possível remover a foto';
-            console.log(err);
-          }
+          err => this.mensagem = err.message
         )
     }
   },
@@ -84,7 +81,7 @@ export default {
 
     this.service
       .lista()
-      .then(fotos => this.fotos = fotos, err => console.log(err));
+      .then(fotos => this.fotos = fotos, err => this.mensagem = err.message);
 
  }
 }
